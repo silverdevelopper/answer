13425aae798817a695de51d3f613f8af5f8d6151
muhammed nurullah gümüş <ngumus@aefis.com>
2021-05-24 13:03:50 +0300

    .

--- answer.py
+++ answer.py
@@ -13,37 +13,28 @@ def isPrime(n:int)->bool:
 
 N = 0
 pyramid = []
-inp = input().strip()
+f = open("input.txt")
+inp = f.readline()
 while (inp != "" and inp != "\n"):
-    pyramid.append([int(x) for x in inp.split()])
-    inp = input().strip()
+    pyramid.append([0 if isPrime(int(x)) else int(x) for x in inp.split()])
+    inp = f.readline()
     N+=1
 
 for i in range(len(pyramid)):   
     for x in range(N-i-1):
         pyramid[i].append(0)
-for p in pyramid:
-    print(p)
 
 def findPathSum(pyr, m, n):
-    for i in range(m-1, -1, -1):
-        for j in range(i+1):
-            if (pyr[i+1][j] > pyr[i+1][j+1]) and not isPrime(pyr[i+1][j]):
-                pyr[i][j] += pyr[i+1][j]
-            elif not isPrime(pyr[i+1][j+1]):
-                pyr[i][j] += pyr[i+1][j+1]
+    temp = pyr.copy()
+    
+    for i in range(m-2, -1, -1):
+        
+        for j in range(n):
+            if pyr[i][j] == 0:
+                continue
+            else:
+                mx = max(temp[i+1][j],temp[i+1][j+1])
+                pyr[i][j] = temp[i][j] + mx
 
     return pyr[0][0]
-print(findPathSum(pyramid, len(pyramid)-1, len(pyramid)-1))
-
-
-
-
-
-
-
-    
-
-   
-
-
+print(findPathSum(pyramid, len(pyramid), len(pyramid)))
