415928c84050c0154ef3b6cfb695d895ae34e778
muhammed nurullah gümüş <ngumus@aefis.com>
2021-05-24 15:26:58 +0300

    .

--- answer.py
+++ answer.py
@@ -1,12 +1,12 @@
 import math
-
+import numpy as np
 
 def isPrime(n:int)->bool:
     if n==1 or n==0:
         return False
     if n==2:
         return True
-    for i in range(2,int(math.sqrt(n)+1)):
+    for i in range(2,int(math.sqrt(n)+1)+1):
         if n%i == 0:
             return False
     return True
@@ -16,7 +16,7 @@ pyramid = []
 f = open("input.txt")
 inp = f.readline()
 while (inp != "" and inp != "\n"):
-    pyramid.append([0 if isPrime(int(x)) else int(x) for x in inp.split()])
+    pyramid.append([-1 if isPrime(int(x)) else int(x) for x in inp.split()])
     inp = f.readline()
     N+=1
 
@@ -25,17 +25,21 @@ for i in range(len(pyramid)):
         pyramid[i].append(0)
 
 def findPathSum(pyr, m, n):
-    temp = pyr.copy()
-    
-    for i in range(m-2, -1, -1):
-        
-        for j in range(n):
-            if pyr[i][j] == 0:
-                continue
-            else:
-                mx = max(temp[i+1][j],temp[i+1][j+1])
-                if mx != 0:
-                    pyr[i][j] = temp[i][j] + mx
+    temp = []
+    for r in pyr:
+        temp.append(r.copy())
 
-    return pyr[0][0]
+    l = 0
+    path = []
+    path.append(temp[0][0])
+    for k in range(m-1):
+        
+        if  temp[k+1][l+1] > temp[k+1][l]:
+            l = l+1
+        if temp[k+1][l] != -1:
+            path.append(temp[k+1][l])
+
+    print("path: "+str(path))
+    return np.sum(path)
+
 print(findPathSum(pyramid, len(pyramid), len(pyramid)))
